b85981aed9f56ee84d933309c1d3ffbfbcdfe4dc
Thor Anderson <ta.anderson@gmail.com>
2020-02-25 12:42:48 -0700

    Added data and images

---
+++ starwars/main.js
@@ -0,0 +1,36 @@
+import { films } from '../data/films.js'
+import { people } from '../data/people.js'
+import { starships } from '../data/starships.js'
+
+const greetingDiv = document.querySelector('.greeting')
+
+const castList = document.createElement("ul")
+
+let counter = 1
+
+people.forEach(person => {
+  let listItem = document.createElement("li")
+  listItem.textContent = person.name
+  castList.appendChild(listItem)
+
+  let anchorWrap = document.createElement("a")
+  anchorWrap.href = "#"
+
+  let imageItem = document.createElement("img")
+  imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+  
+  // add some way to handle user clicks on the image
+  imageItem.addEventListener("click", (event) => {
+    console.log(event)
+  })
+  anchorWrap.appendChild(imageItem)
+  greetingDiv.appendChild(anchorWrap)
+  counter++
+})
+
+greetingDiv.appendChild(castList)
+
+
+
+
+
